365583778f49298023238e8c9bc622d3521d00a9
Rishabh Singh <rishabhsingh50689@gmail.com>
2023-07-29 13:57:03 +0530

    handle Submit

--- client/script.js
+++ client/script.js
@@ -31,10 +31,44 @@ function typeText(element , text) {
   }, 20);
 }
 
-function uniqueID() {
+function generateUniqueID() {
   const timeStamp = Date.now();
   const randomNumber = Math.random();
   const hexadecimal = randomNumber.toString(16);
 
   return `id-${timeStamp}-${hexadecimal}`
 }
+
+
+function chatStripe(isAi , value , uniqueID) {
+  return (
+    `
+      <div class="wrapper ${isAi && "ai"}">
+        <div class = "chat">
+          <div class = "profile">
+            <img src="${isAi ? bot : user}" alt= "${isAi ? 'bot' : 'user'}" />
+          </div>
+          <div class="message" id=${uniqueID}>${value} </div>
+        </div>
+      </div>
+    `
+  )
+}
+
+const  handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form)
+  chatContainer.innerHTML += chatStripe(false,data.get("prompt") )
+   form.reset();
+   const uniqueID = generateUniqueID();
+   chatContainer.innerHTML  += chatStripe(true , "" , uniqueID);
+   chatContainer.scrollTop = chatContainer.scrollHeight;
+   const messageDiv = document.getElementById(uniqueID);
+   loader(messageDiv)
+}
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup" , (e) => {
+  if (e.keyCode === 13 )
+    handleSubmit(e)
+})
